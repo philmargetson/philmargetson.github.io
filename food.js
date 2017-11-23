36055bb6ebc90391815f4d4f68e34459e073f118
philmargetson <phil.margetson@me.com>
2017-11-23 00:25:04 +0000

    Add files via upload

---
+++ food.js
@@ -0,0 +1,6 @@
+var food = ["steak", "BBQ/USA/Burger", "Indian/curry", "chinkehhhh", "thai", "japanese", "lebanese", "mexican", "italian/pizza", "tapas", "pub", "vegetarian(pick again plz)"];
+
+var selected = food[Math.floor(Math.random() * food.length)];
+
+
+alert("selected food is: "+selected);
